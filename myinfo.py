675176eea7db101b75c9cf8cffc16cf0a0d5f5fe
BrynB1 <92232305+BrynB1@users.noreply.github.com>
2022-01-24 12:48:02 -0500

    myinfo Version 1

---
+++ myinfo.py
@@ -0,0 +1,7 @@
+#CS-175-01
+#Bryn Bijur
+#Myinfo
+print("Hello, my name is Bryn.")
+print("My address is 12 Foxhill Run, Monmouth Junction, New Jersey, 08852")
+print("My phone number is 732-278-8330")
+print("I am majoring in software engineering")
